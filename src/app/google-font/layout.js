81e86ab54897dfaf77ca8498ce300f8268cab146
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-06-02 20:35:57 +0300

    using local font

--- src/app/google-font/layout.js
+++ src/app/google-font/layout.js
@@ -1,12 +1,16 @@
-// import needed fonts
-import { public_sans } from "../fonts/font"
-import { roboto } from "../fonts/font"
+//import local font
 
+import localFont from "next/font/local"
 
+// config local font 
 
+const myFont = localFont({
+    src:"../../../public/fonts/Rubik-Italic-VariableFont_wght.ttf",
+    display: "swap",
+})
 export default function GoogleFontLayout({ children }) {
     return (
-        <div className={`${roboto.variable} ${public_sans.variable}`}>
+        <div className={myFont.className}>
 
             {children}
 
